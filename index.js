032e21687b0920234fcab99dd60ac6edd50899b6
Mungai Munene <mungai.munene@gmail.com>
2024-07-11 11:09:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+// Create a variable called random that will generate a random integer greater than 0.
+   const random = Math.ceil(Math.random() * 10);
+   
+
+//  Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+
+    const num3 = 9; 
+    const num4 = 5; 
+    const mod = num3 % num4; 
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
+
+const max = Math.max(1, 5, 15, 12, 20);
